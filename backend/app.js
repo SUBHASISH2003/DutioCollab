a68e3b78454819371d68448d24eaee885228ddac
Saikat Bera <berasaikat731@gmail.com>
2025-05-17 23:42:56 +0530

    edit

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ import morgan from 'morgan'
 export const app = express();
 config({path:".env"});
 
+const allowedOrigin = process.env.FRONTEND_URL || "https://dutio-collab.vercel.app";
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
