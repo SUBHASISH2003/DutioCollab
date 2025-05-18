db2023bf92bb41fae0e14ebb620792a8df7e098a
Saikat Bera <berasaikat731@gmail.com>
2025-05-18 23:36:05 +0530

    edit

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ config({ path: ".env" });
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://dutio-collab.vercel.app",
     credentials: true,
   })
 );
