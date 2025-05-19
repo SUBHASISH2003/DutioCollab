980a5fe4b5a9ee8b81372de8f87cbfe94526a260
Saikat Bera <berasaikat731@gmail.com>
2025-05-19 21:38:42 +0530

    edit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ export const isAuthenticated = catchAsyncError(async (req, res, next) => {
   const token = req.cookies.token;
 
   if (!token) {
-    return next(new ErrorHandler(`User is not authenticated. Token missing. ${token}`, 401));
+    return next(new ErrorHandler(`User is not authenticated. Token missing. with token =  ${token}`, 401));
   }
 
   let decoded;
