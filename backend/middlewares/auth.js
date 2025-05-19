a611920c670841bf3ec7c59aed7d877aa477881d
Saikat Bera <berasaikat731@gmail.com>
2025-05-19 21:08:40 +0530

    edit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,12 +4,28 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
 export const isAuthenticated = catchAsyncError(async (req, res, next) => {
-  const { token } = req.cookies;
+  let token;
+
+  // Check for token in cookies
+  if (req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
+  // Or check for token in Authorization header
+  else if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+
   if (!token) {
     return next(new ErrorHandler("User is not authenticated.", 400));
   }
+
+  // Verify the token
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
+  // Attach user info to req object
   req.user = await User.findById(decoded.id);
 
   next();
