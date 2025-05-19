a21185e2c5b754497a85a723dcabffd7a503aa42
Saikat Bera <berasaikat731@gmail.com>
2025-05-19 20:31:06 +0530

    edit

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ config({ path: ".env" });
 
 app.use(
   cors({
-    origin:"https://dutio-collab.vercel.app",
+    origin:"https://dutio-collab.vercel.app" || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
