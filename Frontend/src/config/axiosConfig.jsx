57340a7835d6d0e64d4ed3af1014609e985471b5
Subhasish Pramanik <pramaniksubhasish28@gmail.com>
2025-06-10 20:36:22 +0530

    ready to host

--- Frontend/src/config/axiosConfig.jsx
+++ Frontend/src/config/axiosConfig.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: "https://dutiocollab-production.up.railway.app", 
+    // baseURL: "https://dutiocollab-production.up.railway.app", 
+    baseURL: "http://localhost:4000", // Use this for local development
     withCredentials: true,
 })
 
